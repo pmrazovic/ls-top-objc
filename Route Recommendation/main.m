141498b6b68bf338ea7c61827101cc5264c5b84f
Petar Mrazovic <petar.mrazovic@gmail.com>
2015-10-14 12:13:58 +0200

    Initial Commit

---
+++ Route Recommendation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Route Recommendation
+//
+//  Created by Pero Mrazovic on 14/10/15.
+//  Copyright © 2015 FrontierCities. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
